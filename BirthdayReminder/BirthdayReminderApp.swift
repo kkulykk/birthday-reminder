e87472aa286ffa3db23b284f4b88ee43f8860952
Roman Kulyk <kkulykk@gmail.com>
2026-02-22 14:05:03 +0100

    Initial Commit

---
+++ BirthdayReminder/BirthdayReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayReminderApp.swift
+//  BirthdayReminder
+//
+//  Created by Roman Kulyk on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
